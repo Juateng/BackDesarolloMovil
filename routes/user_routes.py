d7743ee9b6f7343e480f0de26590f29689f27221
Juan Alberto <jalbertobalam02@gmail.com>
2024-12-06 12:23:48 -0600

    datetime

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,4 +1,4 @@
-
+from datetime import timedelta
 from flask import Blueprint, request, jsonify
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import create_access_token ,jwt_required
@@ -45,8 +45,8 @@ def login():
     user = mongo.db.users.find_one({'email': email})
 
     if user and bcrypt.check_password_hash(user['password'], password):
-        access_token = create_access_token(identity=str(user["_id"]))
-        return jsonify(access_token = access_token), 200
+        access_token = create_access_token(identity=str(user["_id"]), expires_delta=timedelta(days=10))
+        return jsonify(access_token=access_token), 200
     else:
         return jsonify({"msg":"credenciales incorrectas"}), 401
     
